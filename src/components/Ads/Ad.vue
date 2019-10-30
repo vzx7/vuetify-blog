541f49a22a654501be933a52acceff4594cb453a
Vasiliy <coder.vz@gmail.com>
2019-10-30 20:29:51 +0300

    added item page

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -1,24 +1,31 @@
 <template>
-    <v-container grid-list-xs>
-        <v-layout row wrap>
-            <v-flex xs12>
-                <h1>
-                    Home
-                </h1>
-            </v-flex>
-        </v-layout>
-        
-    </v-container>
+  <v-container grid-list-xs>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card>
+          <v-img contain height="300" src="https://bipbap.ru/wp-content/uploads/2017/04/2-16.jpg"></v-img>
+          <v-card-text>
+              <h1 class="text--primary">lorem</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, inventore suscipit. Ratione animi, accusamus labore nostrum obcaecati tenetur error reprehenderit sit magnam. Laborum, delectus cumque? Iste excepturi deserunt possimus id.</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="warning" text>Edit</v-btn>
+            <v-btn class="success">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-    export default {
-        data () {
-            return {}
-        }
-    }
+export default {
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,24 +1,54 @@
 <template>
-    <v-container grid-list-xs>
-        <v-layout row wrap>
-            <v-flex xs12>
-                <h1>
-                    Home
-                </h1>
-            </v-flex>
-        </v-layout>
-        
-    </v-container>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Orders</h1>
+        <v-list subheader two-line flat>
+          <v-list-item-group v-model="settings" multiple>
+            <v-list-item v-for="order in orders" :key="order.id">
+              <template v-slot:default="{ active, toggle }">
+                <v-list-item-action>
+                  <v-checkbox @change="markDown(order)" :input-value="order.done" color="success"></v-checkbox>
+                </v-list-item-action>
+
+                <v-list-item-content>
+                  <v-list-item-title>{{ order.name }}</v-list-item-title>
+                  <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-tile-action>
+                  <v-btn class="primary">Open</v-btn>
+                </v-list-tile-action>
+              </template>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-    export default {
-        data () {
-            return {}
+export default {
+  data() {
+    return {
+      orders: [
+        {
+          id: "asdf",
+          name: "Vasiliy",
+          phone: "345245",
+          adId: 121,
+          done: false
         }
+      ]
+    };
+  },
+  methods: {
+    markDown(order) {
+      order.done = true;
     }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
